3b0d54229e0767cd663a866a12471e1891113d6e
Ian <ikennedy@uw.edu>
2019-01-05 12:16:41 -0800

    updated a local-running version

---
+++ helpers_local.R
@@ -0,0 +1,91 @@
+library(httr)
+#library(rdrop2)
+#library(googlesheets)
+library(readr)
+
+
+# which fields get saved
+fieldsAll <- c('race_ethnicity', 'nationality', 'gender', 'age', "comment", "valence", "rating", "topic", "example", "notes")
+
+# set directory to store responses
+responsesDir <- file.path("responses")
+db_dir <- file.path("db")
+# from local file
+tweet_dir <- file.path("turker_tweets.csv")
+completion_code <- digest::digest(tweet_dir)
+tweet_df = read.csv("turker_tweets.csv", row.names=NULL, colClasses = 'character')
+#Password to login for this session
+
+user <- set_user()
+# give user a random string
+set_user <- function(){
+  user <- digest::digest(humanTime()) # gives the user a hash based on the date and time
+  if(file.exists('users.txt')){
+    if(user %in% read_lines('users.txt')){
+      return(set_user())
+    }
+  }
+  write_lines(user, 'users.txt', append = TRUE)
+  return(user)
+}
+
+
+
+
+
+# re-re
+
+### Generate data here
+###
+###
+###
+
+# add an asterisk to an input label
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+
+# CSS to use in the app
+appCSS <- ".mandatory_star { color: red; }
+.shiny-input-container { margin-top: 25px; }
+.shiny-progress .progress-text {
+font-size: 18px;
+top: 50% !important;
+left: 50% !important;
+margin-top: -100px !important;
+margin-left: -250px !important;
+}"
+ 
+# Helper functions
+humanTime <- function() format(Sys.time(), "%d-%m-%Y-%H-%M-%S")
+
+saveData <- function(data) {
+  fileName <- sprintf("%s_%s.csv",
+                      humanTime(),
+                      digest::digest(data))
+  write.csv(x = data, file = file.path(responsesDir, fileName),
+            row.names = FALSE, quote = TRUE)
+}
+
+epochTime <- function() {
+  as.integer(Sys.time())
+}
+
+round = 10
+
+grab_html <- function(round){
+  tweet_info = GET(paste0("https://publish.twitter.com/oembed?url=",tweet_df$status_url[round],
+                         "?omit_script=TRUE?hide_media=TRUE?hide_thread=TRUE"))
+  tweet_html = content(tweet_info, "parsed")$html
+  return(tweet_html)
+}
+likert = list("Very Negative" = -3, 
+              "Somewhat Negative" = -2,
+              "Mildly Negative" = -1,
+              "Neutral" = 0,
+              "Mildly Positive" = 1,
+              "Somewhat Postive" = 2,
+              "Very Positive" = 3)
--- server.R
+++ server.R
@@ -14,29 +14,22 @@ shinyServer(
     #initialize values
     values <- reactiveValues(round = 1)
     # When the Login button is clicked, check whether user name is in list
-    observeEvent(input$login, {
+    observeEvent(input$complete, {
       
       # User-experience stuff
-      shinyjs::disable("login")
-      start_k <- start_tweet(input$user)
+      shinyjs::disable("complete")
+      start_k <- 0
       values$round <- values$round+start_k
       print(values$round)
-      # Check whether user name and password are correct
-      user_ok <- checkuser(input$user, input$password)
-      
+      user_ok <- TRUE
       # If credentials are valid push user into experiment
       if(user_ok){
-        shinyjs::hide("login_page")
+        shinyjs::hide("demo_page")
         shinyjs::show("form")
         
         # Save username to write into data file
-        output$username <- renderText({input$user})
-      } else {
-        # If credentials are invalid throw error and prompt user to try again
-        shinyjs::reset("login_page")
-        shinyjs::show("login_error")
-        shinyjs::enable("login")
-      }
+        output$username <- renderText({user})}
+ 
       
     })
     
@@ -61,38 +54,11 @@ shinyServer(
     })
     ## This is the main experiment handler
     #Observe the finish button, if cliked, end experiment
-    observeEvent(input$finish_rating, {
-      disable("submit")
-      disable("finish_rating")
-      # Call function formData() (see below) to record submitted response
-      newLine <- isolate(formData())
-      
-      # Write newLine into data frame df
-      isolate({
-        values$df <- rbind(values$df, newLine)
-      })
-      #save the data
-      saveData(values$df)
-      if(values$round==1){
-        output$end_message = renderText(paste0("You've rated ", values$round, ' Tweet, Thank you ', input$user,'!'))
-      } else{
-        output$end_message = renderText(paste0("You've rated ", values$round, ' Tweets, Thank you ', input$user,'!'))
-      }
-      output$response_form = renderText("See you next time!")
-      if(values$round>=dim(tweet_df)[[1]]){
-        output$response_form = renderText('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSclOxuQ5dEX0MYDOhobDiWz1wndGUp6Uf74fuv_6KEQgCaIrw/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>')
-      }
-      update_tweet_count(input$user, values$round)
-      # Say good-bye
-      hide(id = "form")
-      show(id = "end")
-      
-    })
+  
     # Observe the submit button, if clicked... FIRE
     observeEvent(input$submit, {
       disable("submit")
-      disable("finish_rating")
-  
+
       # Call function formData() (see below) to record submitted response
       newLine <- isolate(formData())
       
@@ -108,27 +74,24 @@ shinyServer(
       
       shinyjs::reset('form')
       enable("submit")
-      enable("finish_rating")
       # Are there anymore tweets left?
       # If not then...
       
       if(values$round>dim(tweet_df)[[1]]){
         disable("submit")
-        disable("finish_rating")
         # Call function formData() (see below) to record submitted response
-        newLine <- isolate(formData())
+        # newLine <- isolate(formData())
 
         # Write newLine into data frame df
-        isolate({
-          values$df <- rbind(values$df, newLine)
-        })
+        # isolate({
+        #   values$df <- rbind(values$df, newLine)
+        # })
         #save the data
         saveData(values$df)
-        output$end_message = renderText(paste0('There are no more tweets. You rateed ', values$round, ' Tweets, Thank you!'))
+        output$end_message = renderText(paste0("You're all done with this set!. You rateed ", values$round-1, ' Tweets, Thank you!\n Your completion code is: ', completion_code))
         output$response_form = renderText('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSclOxuQ5dEX0MYDOhobDiWz1wndGUp6Uf74fuv_6KEQgCaIrw/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>')
           
         # Say good-bye
-        update_tweet_count(input$user, values$round)
         hide(id = "form")
         show(id = "end")
       }
@@ -143,7 +106,7 @@ shinyServer(
     # Gather all the form inputs (and add timestamp)
     formData <- reactive({
       data <- sapply(fieldsAll, function(x) input[[x]])
-      data <- c(index = values$round, data, status_id = tweet_df$status_id[values$round], screen_name = tweet_df$screen_name[values$round], timestamp = humanTime())
+      data <- c(index = values$round, user = user, data, status_id = paste0('"',tweet_df$status_id[values$round],'"'), screen_name = tweet_df$screen_name[values$round])
       data <- t(data)
       data
     })
